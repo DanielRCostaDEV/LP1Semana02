38f0e3980b530b9125302c942e8065a8fec0efb3
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-29 20:10:56 +0000

    Height and Radius

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -6,11 +6,17 @@ namespace Cilindro
     {
         static void Main(string[] args)
         {
+            
+            //Height and Radius
             Console.WriteLine("Please, insert the cylinder height: ");
             string h = Console.ReadLine();
 
             Console.WriteLine("Please insert the cylinder radius");
             string rad = Console.ReadLine();
+
+            float height = float.Parse(h);
+            float r = float.Parse(rad);
+
         }
     }
 }
