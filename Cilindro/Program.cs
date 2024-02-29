f47c8df6341c9484c41a655629ebb886d6a45503
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-29 20:05:35 +0000

    Ask for the cylinder height

---
+++ Cilindro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, insert the cylinder height: ");
+            string h = Console.ReadLine();
+        }
+    }
+}
