52c5772f3dd42d9ee6611f3dadb6e7e00ab1a499
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-29 22:41:38 +0000

    4 questions

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Variables
+            string question, response;
+
+            // Ask user a question
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            //Questions and responses
+            switch (question)
+            {
+                case "Do you like git bash?":
+                    response = "What do you think?";
+                    break;
+                
+                case "Will I get a good grade on this assignment?":
+                    response = "Lmao you wish";
+                    break;
+                
+                case "Should I drop out of the course?":
+                    response = "Maybe, but first find out who Gualter is.";
+                    break;
+                
+                case "Who is Gualter?":
+                    response = "@DiogoDeAndrade";
+                    break;
+
+            }
+
+            // respond
+            Console.WriteLine(response);
+        }
+    }
+}
