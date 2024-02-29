336f9dc582be35f2e6cb0a67859bc796140524c3
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-29 19:22:28 +0000

    Asks for a input

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks for a input from user
+            Console.Write("Insert a non negative integer: ");
+            string input = Console.ReadLine();
+
+        }
+    }
+}
