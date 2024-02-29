1f5ef6df841e9465d1ff6ca4acf89a3274acd6c3
Daniel Costa <danielrcosta.dev@gmail.com>
2024-02-29 19:56:05 +0000

    Division, Shift, Xor, >10

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -17,6 +17,12 @@ namespace SmoothOperator
             Console.WriteLine(--n);
             Console.WriteLine(++n);
 
+            //Division, shift, xor, >10
+            Console.WriteLine(n/2);
+            Console.WriteLine(n<<3);
+            Console.WriteLine(n^5);
+            Console.WriteLine(n>10);
+
         }
     }
 }
